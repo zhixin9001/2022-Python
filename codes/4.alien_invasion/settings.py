191613fef7aba676211244a5cad87104078843ff
zhixin <zhixin@zhixins-MacBook-Pro.local>
2022-02-05 14:12:47 +0800

    2.5 ship fire bullet

--- codes/4.alien_invasion/settings.py
+++ codes/4.alien_invasion/settings.py
@@ -3,4 +3,9 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
-    
+        self.ship_speed = 1.5
+        # bullet
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
