f8380684129bce8385e029f2ac2d38e18370cff7
Zhi Xin <zhi.xin@myob.com>
2022-02-22 09:07:37 +0800

    2.22

--- codes/6.data/functions.py
+++ codes/6.data/functions.py
@@ -26,19 +26,6 @@ def get_sum_avg_diff(source, avg_total):
     return (sums, avgs, diffs)
 
 
-def draw_plot(sums, avgs, color='red'):
-    fig, ax = plt.subplots(figsize=(18, 3))
-    ax.plot(sums, color=color)
-    ax.plot(avgs, color='gray')
-    plt.show()
-
-
-def draw_bar(x, y):
-    fig, ax = plt.subplots(figsize=(18, 3))
-    ax.bar(x, y)
-    plt.show()
-
-
 def get_diff_count(diffs, step=10):
     max_value = (max(diffs))
     x_nums = math.ceil(max_value/step)
@@ -52,3 +39,72 @@ def get_diff_count(diffs, step=10):
         diff_count[f'{start}-{end}'] = (len(list(filter(lambda x: x >=
                                                         start and x < end, diffs))))
     return diff_count
+
+
+def get_reds_3area(reds):
+    area1 = list(range(1, 13))
+    area2 = list(range(13, 25))
+    area3 = list(range(25, 36))
+    dic_case = {}
+    for red in reds:
+        area1_count, area2_count, area3_count = 0, 0, 0
+        for boll in red:
+            if (boll in area1):
+                area1_count += 1
+            elif boll in area2:
+                area2_count += 1
+            elif boll in area3:
+                area3_count += 1
+        key = f'{area1_count}-{area2_count}-{area3_count}'
+        value = dic_case.get(key, 0)
+        dic_case[key] = value+1
+    return dic_case
+
+
+def get_reds_4area(reds):
+    area1 = [1, 2, 3, 7, 8, 9, 13, 14, 15]
+    area2 = [4, 5, 6, 10, 11, 12, 16, 17, 18]
+    area3 = [19, 20, 21, 25, 26, 27, 31, 32, 33]
+    area4 = [22, 23, 24, 28, 29, 30, 34, 35]
+
+    dic_case = {}
+    for red in reds:
+        area1_count, area2_count, area3_count, area4_count = 0, 0, 0, 0
+        for boll in red:
+            if (boll in area1):
+                area1_count += 1
+            elif boll in area2:
+                area2_count += 1
+            elif boll in area3:
+                area3_count += 1
+            elif boll in area4:
+                area4_count += 1
+        key = f'{area1_count}-{area2_count}-{area3_count}-{area4_count}'
+        value = dic_case.get(key, 0)
+        dic_case[key] = value+1
+    return dic_case
+
+def get_ac_count(inputs, nums):
+    ac_count = {}
+    for bolls in inputs:
+        diffs = set()
+        for boll1 in bolls:
+            for boll2 in bolls:
+                diffs.add(abs(boll1-boll2))
+        ac = len(diffs)-1-(nums-1)
+        count = ac_count.get(ac, 0)
+        ac_count[ac] = count+1
+    return ac_count
+
+def draw_plot(sums, avgs, color='red'):
+    fig, ax = plt.subplots(figsize=(18, 3))
+    ax.plot(sums, color=color)
+    ax.plot(avgs, color='gray')
+    plt.show()
+
+
+def draw_bar(x, y, rotation=0):
+    fig, ax = plt.subplots(figsize=(18, 3))
+    ax.bar(x, y)
+    plt.xticks(rotation=rotation)
+    plt.show()
